7606686fbe576c883ce8bf48f9bcd972f1e63daf
Mel <angel.melendez.c@gmail.com>
2020-02-11 17:42:25 -0600

    Preparation for first deploy

---
+++ bhealth-api/public/precache-manifest.5ffc6b4428cd8cbc3c33dd3a1842ac1f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0163dfb4e229eb2538e977cb689499b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "522b42fa06251a1ab272",
+    "url": "/static/css/2.f785af3a.chunk.css"
+  },
+  {
+    "revision": "447373e813cea29bb55e",
+    "url": "/static/css/main.e445c343.chunk.css"
+  },
+  {
+    "revision": "522b42fa06251a1ab272",
+    "url": "/static/js/2.1ca1d814.chunk.js"
+  },
+  {
+    "revision": "2a241c7b06711914457ad006040e7ac4",
+    "url": "/static/js/2.1ca1d814.chunk.js.LICENSE"
+  },
+  {
+    "revision": "447373e813cea29bb55e",
+    "url": "/static/js/main.f6f88cc1.chunk.js"
+  },
+  {
+    "revision": "0aac4b18ea19d1faba3a",
+    "url": "/static/js/runtime-main.599b393c.js"
+  },
+  {
+    "revision": "b6b6dbc0c2b65e335d7bab0e1d491446",
+    "url": "/static/media/health-check.b6b6dbc0.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
